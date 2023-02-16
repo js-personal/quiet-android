089b6e30075dde82a5dd9a0c12591295449acedb
0G3NS3C <extenz-@live.com>
2023-02-16 10:10:48 +0100

    stable typescript

--- src/components/rn-sequencer/libs/rn-sequencer.component.tsx
+++ src/components/rn-sequencer/libs/rn-sequencer.component.tsx
@@ -1,69 +1,62 @@
 /************************************
- * 
+ *
  * 2023 Jérémy Sarteur
- *  *
+ *
  */
 
 import type { SetStateAction, MemoExoticComponent } from 'react';
 import React, { Dispatch, memo, useEffect, useMemo, useRef, useState } from 'react';
-import { Animated, Text, View } from 'react-native';
-import type { ISequencer, TEntryFrameProps,TDynamicStyles } from './rn-sequencer.types';
+import { Animated, Text, View, StyleProp, ViewStyle, TextStyle } from 'react-native';
+import type { ISequencer, TEntryFrameProps, TDynamicStyles } from './rn-sequencer.types';
 import SequencerCore from './rn-sequencer';
-import { ReactElement } from 'react';
 
 
 type TEntryBaseSequencerProps = {
-    children: JSX.Element  | undefined;
+    children?: React.ReactNode;
     frames: TEntryFrameProps[];
     infinite?: boolean;
     play?: boolean;
     restartAfterDisable?: boolean;
-    style?: React.CSSProperties;
+    style?: StyleProp<ViewStyle | TextStyle> & TDynamicStyles;
 }
 
-const defaultProps: TEntryBaseSequencerProps = {
+const defaultProps = {
     children: undefined,
     infinite: false,
-    frames: [] as TEntryFrameProps[],
+    frames: [],
     play: true,
     restartAfterDisable: false,
-    style: {} as React.CSSProperties,
+    style: {} as StyleProp<ViewStyle | TextStyle> & TDynamicStyles
 };
 
-
-
-export function useDefaultProps<P extends object>(
-    props: P,
-  ): P & TEntryBaseSequencerProps {
+export function useDefaultProps(props: TEntryBaseSequencerProps ): TEntryBaseSequencerProps{
     return {
-      ...defaultProps,
-      ...props
+        ...defaultProps,
+        ...props
     };
-  }
+}
 
+type SequencerComponent = MemoExoticComponent<React.ForwardRefExoticComponent<TEntryBaseSequencerProps>>
 
-type SequencerComponent<T> = MemoExoticComponent<Animated.AnimatedComponent<T>>
+type TAnimatedWrapper = Animated.AnimatedComponent<TDynamicStyles & React.PropsWithChildren<TSubWrapper>>;
+type TSubWrapper = React.ComponentType<any>;
 
- type TSequencerComponentFactory<A, S> = (AnimatedWrapped: A, SubWrapper: S) => SequencerComponent<S>
+const SequencerComponentFactory = (AnimatedWrapper: TAnimatedWrapper, SubWrapper: TSubWrapper): SequencerComponent =>  {
 
-
-const  SequencerComponentFactory = <A extends React.ComponentType<{}>,S extends React.ComponentType<{}>>(AnimatedWrapper: A, SubWrapper: S): TSequencerComponentFactory<A, S> => {
-
-
-   return React.memo(React.forwardRef((props: TEntryBaseSequencerProps, ref) => {
+    const SequencerComponent = memo(React.forwardRef((props: TEntryBaseSequencerProps, ref) => {
         const { children, frames, infinite, play, restartAfterDisable } = useDefaultProps(props);
-        const opacityValue = useRef(new Animated.Value(0)).current
-        const movementValue = useRef(new Animated.Value(0)).current
-    
-        const [ styles, setStyles ]: [TDynamicStyles, Dispatch<SetStateAction<TDynamicStyles>>] = useState({
+        const opacityValue = useRef(new Animated.Value(0)).current;
+        const movementValue = useRef(new Animated.Value(0)).current;
+
+        const [animatedStyles, setAnimatedStyles]: [TDynamicStyles, Dispatch<SetStateAction<TDynamicStyles>>] = useState({
             opacity: opacityValue,
         } as TDynamicStyles);
-    
-        const sequencer : ISequencer = useMemo(
-            () => new SequencerCore(frames, styles, setStyles, { infinite, restartAfterDisable }, {opacityValue, movementValue}), 
+
+        const sequencer: ISequencer = useMemo(
+            () => new SequencerCore(frames, animatedStyles, setAnimatedStyles, { infinite, restartAfterDisable }, { opacityValue, movementValue }),
             []
         );
-    
+
         useEffect(() => {
             if (play) {
                 sequencer.run();
@@ -71,31 +64,33 @@ const  SequencerComponentFactory = <A extends React.ComponentType<{}>,S extends 
             else {
                 sequencer.stop();
             }
-        }, [play])
+        }, [play]);
 
-        return <AnimatedWrapper style={styles} ref={ref}><SubWrapper style={props.style}>{props.children}</SubWrapper></AnimatedWrapper>
-     }), (prevProps: TEntryBaseSequencerProps, nextProps: TEntryBaseSequencerProps) => prevProps.play === nextProps.play)
-    
-    }
-    
+        return <AnimatedWrapper style={[animatedStyles]} ref={ref}><SubWrapper style={props.style}>{children}</SubWrapper></AnimatedWrapper>;
+    }), (prevProps: TEntryBaseSequencerProps, nextProps: TEntryBaseSequencerProps) => prevProps.play === nextProps.play);
 
- /**
-   * New Sequencer Component
-   * View | Text
-   * @params play = boolean (dynamic)
-   * @params children = JSX.Element
-   * @params frames = TEntryFrameProps[]
-   * @params infinite = boolean
-   * @params restartAfterDisable = boolean
-   */
+    return SequencerComponent;
+};
+
+
+/**
+ * New Sequencer Component
+ * Sequencer.Text | Sequencer.View 
+ * @params play = boolean (dynamic)
+ * @params children = JSX.Element
+ * @params frames = TEntryFrameProps[]
+ * @params infinite = boolean
+ * @params restartAfterDisable = boolean
+ */
+
 type TSequencerTypes = {
-    View: SequencerComponent<View>,
-    Text: SequencerComponent<Text>,
+    View: SequencerComponent,
+    Text: SequencerComponent,
 }
 
 export const SequencerTypes: TSequencerTypes = {
     View: SequencerComponentFactory(Animated.View, View),
-    Text: SequencerComponentFactory(Animated.View, Text),
+    Text: SequencerComponentFactory(Animated.Text, Text),
 }
 
 export default SequencerTypes;
